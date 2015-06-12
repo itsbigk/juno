349f9bfac23ea947e310330875489e219a19c3ed
Gabriel Lew <lew.gabe@gmail.com>
2015-06-12 15:30:57 -0700

    Eliminates redundant code POST test setup code

--- spec/tests/apiRouteTesting.js
+++ spec/tests/apiRouteTesting.js
@@ -2,11 +2,11 @@ var chai   = require('chai'),
 expect     = chai.expect,
 should     = chai.should(),
 Restaurant = require('../../app/config/db/models/restaurant'),
-request = require('supertest'),
-app = require('../../server');
+request = require('supertest');
 
 describe('Routing', function() {
-  var url = 'localhost:3000';
+  request = request('localhost:3000');
+
   describe('POST /api/restaurants', function() {
     var restaurant = null;
     var restaurandId = null;
@@ -24,49 +24,33 @@ describe('Routing', function() {
     });
 
     it('should create restaurant successfully', function(done) {
-      request(url)
+      request
       .post('/api/restaurants')
       .send(restaurant)
       .expect(200)
       .end(function(err, res) {
-        request(url)
+        request
         .delete('/api/restaurants/' + res.body.restaurant_id)
         .end(function(err, res) {
           done();
         });
       });
     });
-  });
-
-  describe('POST duplicate to /api/restaurants', function() {
-    var restaurant = null;
-    before(function() {
-      restaurant = new Restaurant();
-      restaurant.name = "Pepe's Pizza";
-      restaurant.description = "Pizza place";
-      restaurant.cuisine =  "Italian";
-      restaurant.website = "www.pepepizza.com";
-      restaurant.phone = "839-838-1111";
-      restaurant.street =  "8 Pizza Way";
-      restaurant.state =  "California";
-      restaurant.city =  "Los Angeles";
-      restaurant.zip =  90045;
-    });
 
     it('should not create duplicate restaurant', function(done) {
-      request(url)
+      request
       .post('/api/restaurants')
       .send(restaurant)
       .end(function(err, res) {
         var firstRestaurantId = res.body.restaurant_id;
         expect(err).to.be.null;
         expect(res.statusCode).to.equal(200);
-        request(url)
+        request
         .post('/api/restaurants')
         .send(restaurant)
         .end(function(err, res) {
           expect(res.statusCode).to.equal(400);
-          request(url)
+          request
           .delete('/api/restaurants/' + firstRestaurantId)
           .end(function(err, res) {
             done();
@@ -74,5 +58,7 @@ describe('Routing', function() {
         });
       });
     });
+
   });
+
 });
