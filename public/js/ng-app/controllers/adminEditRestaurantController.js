8c75a562a5fb813a546fbb8e21ac877c1361af18
ktk10191 <kyleknott11111@gmail.com>
2015-06-16 11:38:41 -0700

    edit page form validation

--- public/js/ng-app/controllers/adminEditRestaurantController.js
+++ public/js/ng-app/controllers/adminEditRestaurantController.js
@@ -10,24 +10,51 @@ angular.module('Juno')
       $scope.restaurantState = data.state;
     });
 
+    // Clears error elements and error styling
+  function clearErrorStyling() {
+    $(".form-control").removeClass("error");
+    $(".error-message").remove();
+  }
+
   $scope.saveRestaurant = function() {
 
+    console.log($scope.formData);
+
     $scope.processing = true;
+
     $scope.message = '';
 
-    // calling restaurant service to perform an update
     Restaurant.update($stateParams.restaurant_id, $scope.formData)
       .success(function(data) {
+        console.log("success!");
+        console.log("data: ", data);
 
         $scope.processing = false;
 
-        $scope.message = data.message;
+        if (data.errors === undefined) {
+          $scope.message = data.message;
+          // Clears all error styling for new form load
+          clearErrorStyling();
+        }
+        else {
+          // Clear previous errors in case user has fixed some fields
+          clearErrorStyling();
+          // Each key in errors is the name associated with an input ID. Loops through all keys to get all the error fields
+          for (var key in data.errors) {
+            $scope.message = data.message;
+            // Removes unwanted characters from error messages for more user-friendly messages
+            var errorMessage = data.errors[key].message.replace(/Path|`/g, '').trim(),
+                errorElementId = "#" + key;
+            // Checks to make sure that an error message is not applied more than once
+            if ( !$(errorElementId).hasClass("error") ){
+              console.log("has error: " + errorElementId);
+              $(errorElementId + "-container").append("<div class='error-message'>" + errorMessage  + "</div>");
+            }
+            $(errorElementId).addClass("error");
+          }
+        }
       })
-
-      .error(function(err) {
-
-        $scope.processing = false;
-
+      .error(function(err){
         $scope.message = err.message;
       });
   };
