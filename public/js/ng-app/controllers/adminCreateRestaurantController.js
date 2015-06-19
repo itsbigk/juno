26f92a7c6053ecf656cb47fe169178ef899c4c36
7sun <pakrouse@gmail.com>
2015-06-19 10:42:03 -0700

    connects ng-file-upload scripts to app

--- public/js/ng-app/controllers/adminCreateRestaurantController.js
+++ public/js/ng-app/controllers/adminCreateRestaurantController.js
@@ -32,27 +32,27 @@ function createRestaurantController($scope, $state, Restaurant, flash, Upload) {
   }
 
   // Uploads images
-  $scope.$watch('files', function () {
-      $scope.upload($scope.files);
-  });
+  // $scope.$watch('files', function () {
+  //     $scope.upload($scope.files);
+  // });
 
-  $scope.upload = function (files) {
-      if (files && files.length) {
-          for (var i = 0; i < files.length; i++) {
-              var file = files[i];
-              Upload.upload({
-                  url: 'upload/url',
-                  fields: {'username': $scope.username},
-                  file: file
-              }).progress(function (evt) {
-                  var progressPercentage = parseInt(100.0 * evt.loaded / evt.total);
-                  console.log('progress: ' + progressPercentage + '% ' + evt.config.file.name);
-              }).success(function (data, status, headers, config) {
-                  console.log('file ' + config.file.name + 'uploaded. Response: ' + data);
-              });
-          }
-      }
-  };
+  // $scope.upload = function (files) {
+  //     if (files && files.length) {
+  //         for (var i = 0; i < files.length; i++) {
+  //             var file = files[i];
+  //             Upload.upload({
+  //                 url: 'upload/url',
+  //                 fields: {'username': $scope.username},
+  //                 file: file
+  //             }).progress(function (evt) {
+  //                 var progressPercentage = parseInt(100.0 * evt.loaded / evt.total);
+  //                 console.log('progress: ' + progressPercentage + '% ' + evt.config.file.name);
+  //             }).success(function (data, status, headers, config) {
+  //                 console.log('file ' + config.file.name + 'uploaded. Response: ' + data);
+  //             });
+  //         }
+  //     }
+  // };
 
   // Creates new restaurant. Runs when the ng-click function on the view happens
   $scope.saveRestaurant = function() {
