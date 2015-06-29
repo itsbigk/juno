cb6726a53b6f84fb32e56cbf0130d687fbccefa6
Charles Dudley <cd@charlesdudley.com>
2015-06-29 12:11:59 -0700

    took some things out of git that shouldn't me there, added smoothscroll to bower.json, not minifying for now

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,7 @@ uglify           = require('gulp-uglify'),
 mocha            = require('gulp-mocha'),
 jshint           = require('gulp-jshint'),
 ngAnnotate       = require('gulp-ng-annotate'),
-nodemon          = require('gulp-nodemon'); 
+nodemon          = require('gulp-nodemon');
 
 gulp.task('css', function() {
   // processing the less file and outputting it to the same folder as css
@@ -37,7 +37,7 @@ gulp.task('angular', function() {
   .pipe(ngAnnotate())
   .pipe(concat('ngApp.js'))
   .pipe(rename('ngApp.min.js'))
-  .pipe(uglify())
+  // .pipe(uglify())
   .pipe(gulp.dest('public/dist'));
 });
 
